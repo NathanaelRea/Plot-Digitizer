48ec9fb491aa66f71473c6ac74d4d49c8ed8a9f3
OldOxygen <32748805+OldOxygen@users.noreply.github.com>
2018-10-11 04:08:34 +0000

    Converter Overhaul
    Just testing different and much less error prone ways to do this. This is just what I have been working on so far, but as you can see won't work in its current state

--- svg_converter.py
+++ svg_converter.py
@@ -1,83 +1,93 @@
-#!/usr/bin/env python
+#!/usr/bin/python3
 
 # svg_converter.py <svgfile> <xmin> <xmax> <ymin> <ymax>
 #
-# Convert an SVG path and rectangle to a tab-separated list of data
-# points.
+# Convert an SVG path and rectangle to csv type output of notes
 #
-# The reference coordinates given in the arguments correspond to the
-# corners of the rectangle in the SVG file. The SVG file must contain
-# exactly one path and one rectangle (you can leave the reference
-# raster).
+# The reference coordinates given in the arguments correspond to the corners of
+# the rectangle in the SVG file. The SVG can contain any number of paths, and
+# will base the scale off the first rectangle in the file
+# It will output a csv for each csv, separated by newline
 #
-# Copyright (c) OldOxygen (https://github.com/OldOxygen),
-# Michael Danilov (https://github.com/OldOxygen), 2018
-#
-#
-# BUGS
-#
-# * The path handling is extremely flaky and garbages when snaps are used.
-#   Should we use proper SVG library?
+# Contributors
+# OldOxygen         (https://github.com/OldOxygen),
+# Michael Danilov   (https://github.com/mike402)
+
+#import sys
+from xml.dom import minidom
+from svg.path import parse_path
+from argparse import ArgumentParser
 
 
-import sys
+def svg_converter(filename, real_o_x, real_xmax, real_o_y, real_ymax):
+    paths = svg_path_parse(filename)
+    
+    # rectangle
+    # find svg delta_x and delta_y, and xy of origin
+    [svg_o_x, svg_o_y, svg_delta_x, svg_delta_y] = svg_scale(filename)
 
-def main(file, real_o_x, real_xmax, real_o_y, real_ymax):
+    # create scale
+    real_delta_x = real_xmax - real_o_x;
+    real_delta_y = real_ymax - real_o_y;
+    x_scale = real_delta_x / svg_delta_x
+    y_scale = real_delta_y / svg_delta_y
 
-	# svg file to convert
-	svg = read_file(file)
-	# sometimes <path is in defs, just want to skip them
-	svg = svg.replace(find_between(svg,"<defs","</defs>"),"")
+    out = []
+    num_div = input("How many divisions do you want per segment: ")
+    for curve in paths:
+        for i in range(1,num_div):
+            cur_complex = curve.point(1/i)
+            out.append("{},{}\n".format(cur_complex.real,cur_complex.imag))
+    
+    write_file(out_file, out)
 
-	# rectangle
-	# find svg delta_x and delta_y, and xy of origin
-	raw_rectangles = svg.split("<rect\n")
-	svg_delta_x = float(find_between(raw_rectangles[1],"width=\"","\""))
-	svg_delta_y = float(find_between(raw_rectangles[1],"height=\"","\""))
-	svg_o_x = float(find_between(raw_rectangles[1],"x=\"","\""))
-	svg_o_y = float(find_between(raw_rectangles[1],"y=\"","\"")) + svg_delta_y
-
-	# create scale
-	real_delta_x = real_xmax - real_o_x;
-	real_delta_y = real_ymax - real_o_y;
-	x_scale = real_delta_x / svg_delta_x
-	y_scale = real_delta_y / svg_delta_y
-
-	# lines
-	raw_lines = svg.split("<path\n")
-	del raw_lines[0]
-	for line in raw_lines:
-		past = [-svg_o_x, -svg_o_y]
-		coords = find_between(line,"d=\"","\"")
-		coords = coords.replace("m ","").replace("c ","").replace(" z","").split(" ")
-		for coord in coords:
-			tmp = list(map(float, coord.split(",")))
-			tmp[0] += past[0]
-			tmp[1] += past[1]
-			past = list(tmp)
-			tmp[0] *= x_scale
-			tmp[1] *= y_scale
-			print("{}\t{}".format(tmp[0] + real_o_x, - tmp[1] + real_o_y))
+# grabs all paths in svg with attribute 'd'
+# then parses with svg.path
+def svg_path_parse(filename):
+    # https://stackoverflow.com/questions/15857818/python-svg-parser#15857847
+    # icktoofay
+    doc = minidom.parse(svg_file)  # parseString also exists
+    path_strs = [path.getAttribute('d') for path in doc.getElementsByTagName('path')]
+    doc.unlink()
+    
+    for i,p in enumerate(path_strings):
+        path_strings[i] = parse_path(p)
+    
+    return path_strings
 
 
-def read_file(filename):
-	# Simple file read
-	try:
-		file_ = open(filename)
-		txt = file_.read()
-		file_.close()
-		return txt
-	except:
-		input("Cannot find file: {}".format(filename))
+def svg_scale(filename):
+    doc = minidom.parse(svg_file)
+    #get first rectangle element
+    rect = doc.getElementsByTagName('path')[0]
+    x = float(rect.getAttribute('x'))
+    y = float(rect.getAttribute('y'))
+    delta_x = float(rect.getAttribute('width'))
+    delta_y = float(rect.getAttribute('height'))
+    doc.unlink()
+    return [x, y, delta_x, delta_y]
 
-def find_between(s, first, last):
-	# Find substring between two subs in a str (s)
-	try:
-		start = s.index(first) + len(first)
-		end = s.index(last, start)
-		return s[start:end]
-	except ValueError:
-		return ""
+
+#def ui_selection():
+#    pass
 
 if __name__ == "__main__":
-       main(sys.argv[1], float(sys.argv[2]), float(sys.argv[3]), float(sys.argv[4]), float(sys.argv[5]))
+    parser = ArgumentParser()
+    parser.add_argument("-f", "--file", dest="filename", type="str"
+                        help="read SVG FILE", metavar="FILE")
+    parser.add_argument("-o", "--output", dest="output_file", nargs = '?', const="stdout",
+                        help="output FILE", metavar="FILE")
+    #parser.add_argument("-q", "--quiet",
+    #                    action="store_false", dest="verbose", default=True,
+    #                    help="don't print status messages to stdout")
+
+    args = parser.parse_args()
+    
+    print(args)
+    input()
+    
+    if filename == None:
+        files = os.listdir()
+        filename = ui_selection("Pick an svg file to parse:", files, "svg")
+    
+    svg_converter(sys.argv[1], float(sys.argv[2]), float(sys.argv[3]), float(sys.argv[4]), float(sys.argv[5]))
